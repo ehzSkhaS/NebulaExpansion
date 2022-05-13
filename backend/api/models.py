d1e5efdefd379203aa39688a2907284909aa426b
ehzSkhaS <ehz.skhas@gmail.com>
2022-05-13 10:27:57 -0700

    DB Model Design iteration: 3

--- backend/api/models.py
+++ backend/api/models.py
@@ -29,8 +29,7 @@ class Product(models.Model):
     class Meta:
         db_table = 'product'
         verbose_name = "Product"
-        verbose_name_plural = "Products"
-        unique_together = (('code', 'code_provider'),)
+        verbose_name_plural = "Products"        
 
     def __str__(self):
         return self.description
@@ -41,6 +40,8 @@ class Product(models.Model):
 class Warehouse(models.Model):
     number = models.IntegerField(primary_key=True)
     name = models.CharField(null=False, max_length=50)
+    code_product = models.ForeignKey(Product, to_field='code', on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=10, decimal_places=4)
     
     class Meta:
         db_table = 'warehouse'
@@ -49,23 +50,6 @@ class Warehouse(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class WarehouseInventory(models.Model):
-    number_warehouse = models.ForeignKey(Warehouse, primary_key=True, on_delete=models.CASCADE)
-    code_product = models.ForeignKey(Product, to_field='code', on_delete=models.CASCADE)
-    code_provider = models.ForeignKey(Product, to_field='code_provider', on_delete=models.CASCADE)
-    quantity = models.DecimalField(max_digits=10, decimal_places=4)
-    
-    class Meta:
-        db_table = 'warehouse_inventory'
-        verbose_name = "WarehouseInventory"
-        verbose_name_plural = "WarehouseInventories"
-        unique_together = (('code_product', 'code_provider'),)
-        unique_together = (('number_warehouse', 'code_product', 'code_provider'),)
-
-    def __str__(self):
-        return "warehouse number: " + self.number_warehouse + " " +  "code product: " + self.code_product + " " + "code provider: " + self.code_provider
     
 # Store End -------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -88,7 +72,9 @@ class Unit(models.Model):
 class CostCenter(models.Model):
     code = models.IntegerField(primary_key=True)
     name = models.CharField(null=False, max_length=50)
-    code_unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    code_unit = models.ForeignKey(Unit, to_field='code', on_delete=models.CASCADE)
+    code_product = models.ForeignKey(Product, to_field='code', on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=10, decimal_places=4)
 
     class Meta:
         db_table = 'cost_center'
@@ -97,22 +83,6 @@ class CostCenter(models.Model):
 
     def __str__(self):
         return self.name
-
-class CostCenterInventory(models.Model):
-    code_cost_center = models.ForeignKey(CostCenter, on_delete=models.CASCADE)
-    #code_product = models.ForeignKey(Product, to_field='code', related_name='costcenterinventory_product_related', related_query_name='costcenterinventory_product', on_delete=models.CASCADE)
-    #code_provider = models.ForeignKey(Product, to_field='code_provider', related_name='costcenterinventory_product_related', related_query_name='costcenterinventory_product', on_delete=models.CASCADE)
-    quantity = models.DecimalField(max_digits=10, decimal_places=4)    
-    
-    class Meta:
-        db_table = 'cost_center_inventory'
-        verbose_name = "CostCenterInventory"
-        verbose_name_plural = "CostCenterInventories"
-        #unique_together = (('code_product', 'code_provider'),)
-        #unique_together = (('code_cost_center', 'code_product', 'code_provider'),)
-
-    #def __str__(self):
-     #   return "code cost center: " + self.code_cost_center + " " +  "code product: " + self.code_product + " " + "code provider: " + self.code_provider
 
 # Client End ------------------------------------------------------------------------------------------------------------------------------------------------
     
