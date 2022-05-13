34c8706ee30d08d2f074b93db8b4a2dfacf319c9
ehzSkhaS <ehz.skhas@gmail.com>
2022-05-13 13:14:43 -0700

    DB Model Design iteration: 4

--- backend/api/models.py
+++ backend/api/models.py
@@ -18,13 +18,13 @@ class Provider(models.Model):
 
     
 class Product(models.Model):
-    code = models.IntegerField(primary_key=True)
-    code_provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    code = models.IntegerField(primary_key=True)    
     account = models.IntegerField(null=False)
     account_sub = models.IntegerField(null=False)
     description = models.CharField(null=False, max_length=50)
     unit = models.CharField(null=False, max_length=10)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    code_provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'product'
@@ -39,9 +39,9 @@ class Product(models.Model):
 
 class Warehouse(models.Model):
     number = models.IntegerField(primary_key=True)
-    name = models.CharField(null=False, max_length=50)
-    code_product = models.ForeignKey(Product, to_field='code', on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=50)    
     quantity = models.DecimalField(max_digits=10, decimal_places=4)
+    code_product = models.ForeignKey(Product, to_field='code', on_delete=models.CASCADE)
     
     class Meta:
         db_table = 'warehouse'
@@ -71,10 +71,10 @@ class Unit(models.Model):
     
 class CostCenter(models.Model):
     code = models.IntegerField(primary_key=True)
-    name = models.CharField(null=False, max_length=50)
+    name = models.CharField(null=False, max_length=50)    
+    quantity = models.DecimalField(max_digits=10, decimal_places=4)
     code_unit = models.ForeignKey(Unit, to_field='code', on_delete=models.CASCADE)
     code_product = models.ForeignKey(Product, to_field='code', on_delete=models.CASCADE)
-    quantity = models.DecimalField(max_digits=10, decimal_places=4)
 
     class Meta:
         db_table = 'cost_center'
@@ -85,4 +85,33 @@ class CostCenter(models.Model):
         return self.name
 
 # Client End ------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Operations ------------------------------------------------------------------------------------------------------------------------------------------------
+
+class DeliverySC208(models.Model):
+    voucher = models.AutoField(primary_key=True, unique_for_year="date_stamp", editable=False)
+    request_voucher_1 = models.IntegerField(null=False)
+    request_voucher_2 = models.IntegerField(null=False)
+    request_plan = models.IntegerField(null=False)
+    quantity = models.DecimalField(max_digits=10, decimal_places=4)
+    existence = models.DecimalField(max_digits=10, decimal_places=4)
+    ci_cost_center_receiver = models.IntegerField(null=False)
+    ci_warehouse_dispatcher = models.IntegerField(null=False)
+    date_stamp = models.DateField(auto_now=True, editable=False)
+    # I have doubts with the realtion of code_product should be with product
+    # or with Warehouse, but I can't hace 2 fields refering the same table
+    code_product = models.ForeignKey(Product, to_field='code', on_delete=models.CASCADE)
+    number_warehouse = models.ForeignKey(Warehouse, to_field='number', on_delete=models.CASCADE)
+    code_cost_center = models.ForeignKey(CostCenter, to_field='code', on_delete=models.CASCADE)
     
+
+    class Meta:
+        db_table = 'delivery_sc_2_08'
+        verbose_name = "DeliverySC208"
+        verbose_name_plural = "DeliverySC208s"
+
+    def __str__(self):
+        return self.voucher
+
+# Operations End --------------------------------------------------------------------------------------------------------------------------------------------
